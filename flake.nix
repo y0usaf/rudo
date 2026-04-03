470dc45f4cbf7df8a072fac3b0859dc55b7ebdd6
y0usaf <OA99@Outlook.com>
2026-04-03 15:10:18 -0400

    Add working flake app and move repo to root

--- flake.nix
+++ flake.nix
@@ -10,47 +10,60 @@
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
+        runtimeLibs = with pkgs; [
+          fontconfig
+          freetype
+          libGL
+          libxkbcommon
+          wayland
+          libx11
+          libxcursor
+          libxi
+          libxrandr
+          libxext
+          libxinerama
+          libxcb
+        ];
+        nativeTools = with pkgs; [
+          pkg-config
+          clang
+          cmake
+          ninja
+          python3
+          makeWrapper
+        ];
+        termvide = pkgs.writeShellApplication {
+          name = "termvide";
+          runtimeInputs = with pkgs; [
+            cargo
+            rustc
+            gcc
+          ] ++ nativeTools ++ runtimeLibs;
+          text = ''
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeLibs}:''${LD_LIBRARY_PATH:-}"
+            export LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeLibs}:''${LIBRARY_PATH:-}"
+            export CARGO_TARGET_DIR="''${XDG_CACHE_HOME:-$HOME/.cache}/termvide/target"
+            mkdir -p "$CARGO_TARGET_DIR"
+            exec cargo run --manifest-path ${./.}/Cargo.toml -- "$@"
+          '';
+        };
       in {
+        packages.default = termvide;
+        apps.default = flake-utils.lib.mkApp {
+          drv = termvide;
+          name = "termvide";
+        };
+
         devShells.default = pkgs.mkShell {
           packages = with pkgs; [
             cargo
             rustc
             rustfmt
             clippy
-            pkg-config
-            clang
-            cmake
-            ninja
-            python3
-            fontconfig
-            freetype
-            libGL
-            libxkbcommon
-            wayland
-            libx11
-            libxcursor
-            libxi
-            libxrandr
-            libxext
-            libxinerama
-            libxcb
-          ];
+          ] ++ nativeTools ++ runtimeLibs;
 
           shellHook = ''
-            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath [
-              pkgs.fontconfig
-              pkgs.freetype
-              pkgs.libGL
-              pkgs.libxkbcommon
-              pkgs.wayland
-              pkgs.libx11
-              pkgs.libxcursor
-              pkgs.libxi
-              pkgs.libxrandr
-              pkgs.libxext
-              pkgs.libxinerama
-              pkgs.libxcb
-            ]}:$LD_LIBRARY_PATH"
+            export LD_LIBRARY_PATH="${pkgs.lib.makeLibraryPath runtimeLibs}:$LD_LIBRARY_PATH"
             echo "Entered Termvide dev shell"
             echo "Run: cargo run"
           '';
