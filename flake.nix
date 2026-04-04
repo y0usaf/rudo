671d3a4ab974aa23165f7a7e23002702bfe84830
y0usaf <OA99@Outlook.com>
2026-04-03 23:28:10 -0400

    build(nix): package termvide with crane

--- flake.nix
+++ flake.nix
@@ -4,113 +4,138 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    crane.url = "github:ipetkov/crane";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        lib = pkgs.lib;
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    crane,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      lib = pkgs.lib;
 
-        runtimeLibs = with pkgs; [
-          fontconfig
-          freetype
-          libGL
-          libxkbcommon
-          wayland
-          libx11
-          libxcursor
-          libxi
-          libxrandr
-          libxext
-          libxinerama
-          libxcb
-        ];
+      runtimeLibs = with pkgs; [
+        fontconfig
+        freetype
+        libGL
+        libxkbcommon
+        wayland
+        libx11
+        libxcursor
+        libxi
+        libxrandr
+        libxext
+        libxinerama
+        libxcb
+      ];
 
-        nativeTools = with pkgs; [
+      useMold = pkgs.stdenv.isLinux;
+      rustLinkEnv = lib.optionalAttrs useMold {
+        CARGO_BUILD_RUSTFLAGS = "-C linker=clang -C link-arg=-fuse-ld=${pkgs.mold}/bin/mold";
+      };
+
+      nativeTools = with pkgs;
+        [
           pkg-config
           clang
           cmake
           ninja
           python3
           makeWrapper
+        ]
+        ++ lib.optionals useMold [
+          pkgs.mold
         ];
 
-        packageNativeBuildInputs = [
-          pkgs.makeWrapper
-          pkgs.pkg-config
-          pkgs.python3
-          pkgs.rustPlatform.bindgenHook
-          pkgs.removeReferencesTo
-        ];
+      packageNativeBuildInputs = [
+        pkgs.makeWrapper
+        pkgs.pkg-config
+        pkgs.python3
+        pkgs.rustPlatform.bindgenHook
+        pkgs.removeReferencesTo
+      ];
 
-        skiaSourceDir =
-          let
-            repo = pkgs.fetchFromGitHub {
-              owner = "rust-skia";
-              repo = "skia";
-              tag = "m145-0.92.0";
-              hash = "sha256-9N780AwheKBJRcZC4l/uWFNq+oOyoNp4M6dJAVVAFeo=";
-            };
-            externals = pkgs.linkFarm "skia-externals"
-              (lib.mapAttrsToList (name: value: {
-                inherit name;
-                path = pkgs.fetchgit value;
-              }) (builtins.fromJSON (builtins.readFile ./skia-externals.json)));
-          in
-          pkgs.runCommand "termvide-skia-source" { } ''
-            cp -R ${repo} $out
-            chmod -R +w $out
-            ln -s ${externals} $out/third_party/externals
-          '';
+      skiaSourceDir = let
+        repo = pkgs.fetchFromGitHub {
+          owner = "rust-skia";
+          repo = "skia";
+          tag = "m145-0.92.0";
+          hash = "sha256-9N780AwheKBJRcZC4l/uWFNq+oOyoNp4M6dJAVVAFeo=";
+        };
+        externals =
+          pkgs.linkFarm "skia-externals"
+          (lib.mapAttrsToList (name: value: {
+            inherit name;
+            path = pkgs.fetchgit value;
+          }) (builtins.fromJSON (builtins.readFile ./skia-externals.json)));
+      in
+        pkgs.runCommand "termvide-skia-source" {} ''
+          cp -R ${repo} $out
+          chmod -R +w $out
+          ln -s ${externals} $out/third_party/externals
+        '';
 
-        termvide = pkgs.rustPlatform.buildRustPackage.override {
-          stdenv = pkgs.clangStdenv;
-        } {
-          pname = "termvide";
-          version = "0.16.0";
-          src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
+      craneLib = crane.mkLib pkgs;
 
-          env = {
+      commonArgs = {
+        pname = "termvide";
+        version = "0.16.0";
+        src = lib.cleanSource ./.;
+        stdenv = p: p.clangStdenv;
+        strictDeps = true;
+
+        env =
+          {
             SKIA_SOURCE_DIR = skiaSourceDir;
             SKIA_GN_COMMAND = "${pkgs.gn}/bin/gn";
             SKIA_NINJA_COMMAND = "${pkgs.ninja}/bin/ninja";
-          };
+          }
+          // rustLinkEnv;
 
-          nativeBuildInputs = packageNativeBuildInputs;
+        nativeBuildInputs = packageNativeBuildInputs;
+        buildInputs = [pkgs.SDL2 pkgs.fontconfig] ++ runtimeLibs;
 
-          buildInputs = [ pkgs.SDL2 pkgs.fontconfig ] ++ runtimeLibs;
+        postPatch = ''
+          for path in $(find . /build/cargo-vendor-dir -path '*/skia-bindings-0.93.1/src/bindings.cpp' 2>/dev/null); do
+            substituteInPlace "$path" \
+              --replace '#include "include/effects/SkGradient.h"' ""
+          done
+        '';
 
-          doCheck = false;
+        doCheck = false;
+      };
 
-          postPatch = ''
-            for path in $(find . /build/cargo-vendor-dir -path '*/skia-bindings-0.93.1/src/bindings.cpp' 2>/dev/null); do
-              substituteInPlace "$path" \
-                --replace '#include "include/effects/SkGradient.h"' ""
-            done
-          '';
+      cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+      termvide = craneLib.buildPackage (commonArgs
+        // {
+          inherit cargoArtifacts;
 
           postFixup = ''
             remove-references-to -t "$SKIA_SOURCE_DIR" $out/bin/termvide || true
             wrapProgram $out/bin/termvide \
               --prefix LD_LIBRARY_PATH : ${lib.makeLibraryPath runtimeLibs}
           '';
-        };
-      in {
-        packages.default = termvide;
-        apps.default = flake-utils.lib.mkApp {
-          drv = termvide;
-          name = "termvide";
-        };
+        });
+    in {
+      packages.default = termvide;
+      apps.default = flake-utils.lib.mkApp {
+        drv = termvide;
+        name = "termvide";
+      };
 
-        devShells.default = pkgs.mkShell {
-          packages = with pkgs; [
-            cargo
-            rustc
-            rustfmt
-            clippy
-          ] ++ nativeTools ++ runtimeLibs;
+      devShells.default = pkgs.mkShell ({
+          packages = with pkgs;
+            [
+              cargo
+              rustc
+              rustfmt
+              clippy
+            ]
+            ++ nativeTools ++ runtimeLibs;
 
           shellHook = ''
             export LD_LIBRARY_PATH="${lib.makeLibraryPath runtimeLibs}:''${LD_LIBRARY_PATH:-}"
@@ -118,6 +143,7 @@
             echo "Entered Termvide dev shell"
             echo "Run: cargo run"
           '';
-        };
-      });
+        }
+        // rustLinkEnv);
+    });
 }
